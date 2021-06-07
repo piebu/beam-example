6703b1fbe6b057d693a467df5fd0f6b1f7d49ba5
Piero Btz <piero.buttazzo@smithhotels.com>
2021-06-07 23:15:39 +0100

    added pipeline options and fixed DirectRunner

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from datetime import date
 import argparse
 
 from apache_beam.io.filesystem import CompressionTypes
+from apache_beam.options.pipeline_options import PipelineOptions
 
 
 def convert_string_to_time(time_string):
@@ -28,9 +29,6 @@ def check_args(app_args):
     if app_args.min_amount is None or float(app_args.min_amount) < 0:
         print("Parameter min_amount>=0")
         exit(-1)
-    if app_args.input_file is None:
-        print("Parameter --input_file is required")
-        exit(-1)
     return {
         "year_to_remove": int(app_args.year),
         "min_amount": float(app_args.min_amount),
@@ -41,16 +39,24 @@ def check_args(app_args):
 parser = argparse.ArgumentParser()
 parser.add_argument('--year', default=2010, help='Year to not consider format like 2010')
 parser.add_argument('--min_amount', default=10, help='Minimum transaction amount')
-parser.add_argument('--input_file', help='Csv transaction file url')
-args = parser.parse_args()
+parser.add_argument(
+    '--input-file',
+    default='gs://cloud-samples-data/bigquery/sample-transactions/transactions.csv',
+    help='The file path for the input text to process.')
+
+args, beam_args = parser.parse_known_args()
 app_param = check_args(args)
 
+beam_options = PipelineOptions(
+    beam_args,
+    runner='DirectRunner'
+)
 
 # Running locally in the DirectRunner.
-with beam.Pipeline() as pipeline:
+with beam.Pipeline(None, beam_options) as pipeline:
     (
         pipeline
-        | 'Read lines' >> beam.io.ReadFromText(app_param.get("input_file"), skip_header_lines=1)
+        | 'Read lines' >> beam.io.ReadFromText(args.input_file, skip_header_lines=1)
         | 'Split lines' >> beam.Map(lambda line: split_and_enrich_with_time(line))
         | 'Filter lines' >> beam.Filter(lambda line: filter_transaction(line, app_param))
         | 'Create the pair' >> beam.Map(lambda transaction: (time.strftime("%Y-%m-%d", transaction[0]), transaction[1]))
